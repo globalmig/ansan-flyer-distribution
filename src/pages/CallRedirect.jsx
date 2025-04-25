3f2cae7eb177f268227ac40533444c60ff9ba4ff
연구개발전담부 <wdpb@mig.kr>
2025-04-25 15:27:36 +0900

    fix: 표기 번호 변경

--- src/pages/CallRedirect.jsx
+++ src/pages/CallRedirect.jsx
@@ -31,7 +31,7 @@ export function CallRedirect() {
       <div className="flex flex-col items-center p-6 bg-white rounded-lg  max-w-sm mx-auto mt-20">
         <p className="text-lg font-semibold text-center">전화 연결은 모바일에서 가능합니다.</p>
         <p className="mt-2 text-gray-600 text-sm text-center">아래 번호로 직접 연락해주세요.</p>
-        <p className="mt-3 text-xl font-bold text-blue-600">010-4960-2002</p>
+        <p className="mt-3 text-xl font-bold text-blue-600">010-4509-8153</p>
         <Link to="/" className="mt-4">
           <ButtonDefault text="홈으로" />
         </Link>
