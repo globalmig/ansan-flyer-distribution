d8de0ca688c29a0afaba54575208c3b2ce9afe7e
연구개발전담부 <wdpb@mig.kr>
2025-04-25 16:33:28 +0900

    fix:bnb mt값 감소

--- src/components/Ask.jsx
+++ src/components/Ask.jsx
@@ -7,7 +7,7 @@ export function Ask() {
     <div className="flex flex-col justify-center items-center  py-10 w-full bg-[#F9F9FA] mt-20 mx-4">
       <div className="flex flex-col items-center justify-center text-subtitle mx-4">
         <p className="text-2xl font-semibold mb-4">도움이 필요하신가요?</p>
-        <p className="font-light text-center">인쇄부터 배포까지 한 번에! 지금 문의하세요</p>
+        <p className="font-light text-center">인쇄부터 배포까지 한 번에! 지금 문의하세요!</p>
       </div>
       {/* 숨겨진 input 필드 추가 */}
       <input type="text" id="copyNumber" value="010-4960-2002" readOnly className="absolute opacity-0" />
