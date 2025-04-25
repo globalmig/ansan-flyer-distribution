84692cbb48c13adf5c37d77316ba14f0674e5d52
연구개발전담부 <wdpb@mig.kr>
2025-04-25 15:52:16 +0900

    fix: 예외처리, call 화면 레이아웃 정돈

--- src/pages/CallRedirect.jsx
+++ src/pages/CallRedirect.jsx
@@ -28,22 +28,22 @@ export function CallRedirect() {
   if (!isMobile) {
     // 데스크톱일 경우 전화번호를 안내
     return (
-      <div className="flex flex-col items-center p-6 bg-white rounded-lg  max-w-sm mx-auto mt-20">
+      <div className="flex flex-col text-center justify-center items-center h-screen max-h-[600px]">
         <p className="text-lg font-semibold text-center">전화 연결은 모바일에서 가능합니다.</p>
         <p className="mt-2 text-gray-600 text-sm text-center">아래 번호로 직접 연락해주세요.</p>
         <p className="mt-3 text-xl font-bold text-blue-600">010-4509-8153</p>
         <Link to="/" className="mt-4">
-          <ButtonDefault text="홈으로" />
+          <ButtonDefault text="메인화면 이동" />
         </Link>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg max-w-sm mx-auto mt-20">
+    <div className="flex flex-col text-center justify-center items-center h-screen max-h-[600px]">
       <p className="text-lg text-center">전화 연결 중입니다...</p>
       <Link to="/" className="mt-4">
-        <ButtonDefault text="홈으로" />
+        <ButtonDefault text="메인화면 이동" />
       </Link>
     </div>
   );
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 export function NotFoundPage() {
   return (
-    <div className="flex flex-col text-center mt-24">
+    <div className="flex flex-col text-center justify-center items-center h-screen max-h-[600px] ">
       <h1>404 - 페이지를 찾을 수 없습니다.</h1>
       <p>존재하지 않는 주소입니다.</p>
 
       <Link to="/">
-        <ButtonDefault text="메인화면으로 이동" />
+        <ButtonDefault text="메인화면 이동" />
       </Link>
     </div>
   );
